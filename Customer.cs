9b5f52962051f42b3770086f84e31c792ed8967e
Vinod Kumar Tewthia <vktewthia@gmail.com>
2024-02-26 16:09:32 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NRules.Fluent.Dsl;
+
+namespace DemoNRules
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public bool IsPreferred { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void NotifyAboutDiscount()
+        {
+            Console.WriteLine($"Customer {Name} was notified about a discount");
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; }
+        public Customer Customer { get; }
+        public int Quantity { get; }
+        public double UnitPrice { get; }
+        public double PercentDiscount { get; set; }
+        public bool IsOpen { get; set; } = true;
+
+        public Order(int id, Customer customer, int quantity, double unitPrice)
+        {
+            Id = id;
+            Customer = customer;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+
+    public class PreferredCustomerDiscountRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = default;
+            IEnumerable<Order> orders = default;
+
+            When()
+                .Match<Customer>(() => customer, c => c.IsPreferred)
+                .Query(() => orders, x => x
+                    .Match<Order>(
+                        o => o.Customer == customer,
+                        o => o.IsOpen,
+                        o => o.PercentDiscount == 0.0)
+                    .Collect()
+                    .Where(c => c.Any()));
+
+            Then()
+                .Do(ctx => ApplyDiscount(orders, 10.0))
+                .Do(ctx => ctx.UpdateAll(orders));
+        }
+
+        private static void ApplyDiscount(IEnumerable<Order> orders, double discount)
+        {
+            foreach (var order in orders)
+            {
+                order.PercentDiscount = discount;
+            }
+        }
+    }
+
+    public class DiscountNotificationRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = default;
+
+            When()
+                .Match<Customer>(() => customer)
+                .Exists<Order>(o => o.Customer == customer, o => o.PercentDiscount > 0.0);
+
+            Then()
+                .Do(_ => customer.NotifyAboutDiscount());
+        }
+    }
+}
